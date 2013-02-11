57ad49ecf32c93117b4e63130358aaff404f486d
Brandon Ganch <Brandon.D.Ganch@Dartmouth.edu>
2013-02-11 11:50:36 -0500

    Updated tests to account for new return parameters

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,14 +3,16 @@ require 'rms_api/event'
 
 describe RMSAPI::Event do
   describe "#new" do
-    let(:row) { { 'EVENT_ID' => '2012-AAXKVA201301081900@r25.dartmouth.edu', 'START_TIME' => "2012-DEC-25 19:01",
-                  'LOCATION' =>'Moore Theater', 'CONTACT_NAME' => 'Box Office'} }
+    let(:row) { { 'EVENT_ID' => '2012-AAXKVA201301081900@r25.dartmouth.edu', 'START_TIME' => "2012-DEC-25 19:01", 'END_TIME' => "2012-DEC-25 20:00",
+                  'EVENT_TITLE' => 'Christmas Revels', 'LOCATION' =>'Moore Theater', 'CONTACT_NAME' => 'Box Office'} }
 
     subject { RMSAPI::Event.new(row) }
 
     context "parsing Event" do
       its(:rms_id) { should == '2012-AAXKVA201301081900@r25.dartmouth.edu' }
       its(:start_time) { should == '2012-DEC-25 19:01' }
+      its(:end_time) { should == '2012-DEC-25 20:00' }
+      its(:title) { should == 'Christmas Revels' }
       its(:location) { should == 'Moore Theater' }
       its(:contact_name) { should == 'Box Office' }
     end
